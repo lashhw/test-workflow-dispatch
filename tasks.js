da4c22845053e8cfaa7d782ae3e6653aa7f4471f
Yen-Chieh Huang <lashhw81@gmail.com>
2026-06-24 20:13:32 +0800

    add option

---
+++ tasks.js
@@ -0,0 +1,16 @@
+window.workflowTasks = {
+  task_1: {
+    label: "task_1",
+    option: [
+      { value: "small", label: "small" },
+      { value: "large", label: "large" }
+    ]
+  },
+  task_2: {
+    label: "task_2",
+    option: [
+      { value: "quick", label: "quick" },
+      { value: "long", label: "long" }
+    ]
+  }
+};
